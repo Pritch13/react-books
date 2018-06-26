a5c17adb376a13fdcdbe777f4794a92e81a087fb
Sam Pritchard <sam@Sams-MacBook-Pro.local>
2018-06-26 13:50:30 -0600

    first

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from "react-router-dom";
+
+
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+    <Switch>
+        <Route exact path="/" component={Books} />
+        <Route exact path="/books" component={Books} />
+        <Route path="/books/:id" component={Detail} />
+        <Route path="/" exact component={Books} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
